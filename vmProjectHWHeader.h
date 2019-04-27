b873de597daaba8725885c79c4297b90b7d527b9
rogergriff <rogergriff121@gmail.com>
2019-04-27 15:40:03 -0700

    Update vmProjectHWHeader.h

--- vmProjectHWHeader.h
+++ vmProjectHWHeader.h
@@ -1,1 +1,21 @@
+//header file for the hardware portion
+class HW{
+};
 
+class MMU: public HW{
+}
+
+class Word: public HW{
+};
+
+class Adr: public HW{
+};
+
+class BS:public HW{
+};
+
+class RAM: public HW{
+};
+
+class status: public RAM{
+};
