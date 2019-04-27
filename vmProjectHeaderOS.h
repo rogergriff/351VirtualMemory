bf2f6163ec6cc7d38cac73c15bb5531cc2895cdb
rogergriff <rogergriff121@gmail.com>
2019-04-27 15:36:32 -0700

    Update vmProjectHeaderOS.h

--- vmProjectHeaderOS.h
+++ vmProjectHeaderOS.h
@@ -1,1 +1,21 @@
---this will be the header file for the OS part our project
+//this will be the header file for the OS part our project
+class OS{
+};
+class PRA: public OS{ //Page replacement algorithm
+};
+
+
+class FIFO: public PRA{
+};
+
+class LRU: public PRA{
+};
+
+class MM: public OS{ //Memory Manager for OS
+};
+
+class PTE: public OS{ //Page Table Entry
+};
+
+class PCB: public OS{ //Process control block
+};
