cb1243b22aa4b79b3938c910dbe7fc9f04b19179
rogergriff <rogergriff121@gmail.com>
2019-04-27 17:34:09 -0700

    Update vmProjectHWHeader.h

--- vmProjectHWHeader.h
+++ vmProjectHWHeader.h
@@ -3,17 +3,44 @@ class HW{
 };
 
 class MMU: public HW{ //Memory Management Unit
+private:
+  int page_acc_count;
+  int page_faults;
+  tlb; //The actual tlb i think
+  int tlb_acc_count; //how many times accessed
+  int tlb_faults; //how many faults
+public:
+  void clearTLB(); // clears the TLB
+  instance(); // ??? no idea what it does, but its on the hints, maybe creates tlb?
+  MMU(); // constructor
+  void pageAccesses(); //how many pages were accessed? pretty sure a simple cout
+  void pageFaults(); //how many page faults where there? pretty sure a simple cout
+  read<T>;//read from standard input the 32 bit integer numbers that represent logical addresses 
+  tlbAccesses(); //how many times did the TLB get accessed? pretty sure just a simple cout
+  tlbFaults(); //how many times was there a fault accessing?  ^
 }
 
 class PageFault: public MMU{ 
+public:
+  PageFault();
+  Word pageNumber;
 };
-class Word: public HW{
+struct Word: public HW{
+  int uint32_t(); // make the value i think?
+  uint32_t value;
 };
 
-class Adr: public Word{ //Address
+struct Adr: public Word{ //Address
+Word Adr(); //implement it
+Word displacement();
+Word frame();
+Word page();  //
 };
 
 class BS:public HW{ //Backing Store
+private:
+  BS(); //constructor
+  media
 };
 
 class RAM: public HW{
