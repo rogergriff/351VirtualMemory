897d32060c5bfd822dcf3802e2bbf9f2b71c1c78
life-lenses <lgomez420@csu.fullerton.edu>
2019-05-05 16:28:12 -0700

    Update virtualmemorymanger.cpp

--- virtualmemorymanger.cpp
+++ virtualmemorymanger.cpp
@@ -6,6 +6,70 @@
 #include <fstream>
 using namespace std;
 
+int main() {
 
+  
+  // Header OS
+  
+// protected Class PRA
+  
 
+// From Class PRA
+  
+PRA::~PRA();
 
+Word PRA::findVictim (){
+  
+};
+
+Word PRA::getPRA (){
+  
+}; 
+
+Word PRA::updateUsage(){
+  
+};
+
+  
+// from struct FIFO in Class PRA
+
+Word PRA FIFO::findVictim(){
+
+};
+  
+Word PRA FIFO::FIFO(); 
+ 
+//From class LRU:public PRA
+  
+Word LRU::findVictim(){
+  
+};
+  
+LRU::LRU ();
+  
+void LRU::updateUsage(){
+
+};
+  
+//From Class Memory Manager  
+  
+MM::MM ();
+  
+MM MM::instance(){
+};
+  
+bool MM::operator=(){
+};
+  
+void MM::pageIN(pageTable){
+};
+  
+  
+ 
+  
+  
+  
+  
+  
+  
+}
