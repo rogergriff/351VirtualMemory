2c282a54427abc33fafbd5da664d524082c3ecff
life-lenses <lgomez420@csu.fullerton.edu>
2019-05-05 14:05:03 -0700

    Update vmProjectHeaderOS.h

--- vmProjectHeaderOS.h
+++ vmProjectHeaderOS.h
@@ -1,6 +1,5 @@
 #ifndef OS_H
 #define OS_H
-
 #include <iostream>
 #include <fstream>
 using namespace std;
@@ -12,28 +11,45 @@ class OS{
 }; */
 
 class PRA{ //Page replacement algorithm
-private:
-  int options(); //use this to get the type of PRA wanted at run-time, so something like "what page replacemnt algorithm do you want to use, 1 for fifo or 2 for lru
-protected:
-    PRA(); //constructor for class
-public:
- ~PRA(); //destructor
- Word findVictim();
- getPRA();
- updateUsage();
-};
+      struct FIFO {
+        public:
+           Word findVictim();
+           FIFO(); // constructor
+        private:
+           Word front; // what's at the front and will therefore be booted next when it's full.
+      } 
+      class LRU {
+         public:
+           Word findVictim();
+           LRU();
+           void updateUsage();
+        private:
+           LRUList;
+       }
+        
+    private:
+          int options(); //use this to get the type of PRA wanted at run-time, so something like "what page replacemnt algorithm do you want to use, 1 for fifo or 2 for lru
+    protected:
+          PRA(); //constructor for class
+    public:
+          ~PRA(); //destructor
+           Word findVictim();
+           getPRA();
+           updateUsage();
+  };
 
 
-struct FIFO: public PRA{
+/* struct FIFO: public PRA{
 public:
   Word findVictim();
-  FIFO(); //constructor
+  FIFO(); //constructor 
   
 private:
   Word front; //what's at the front and will therefore be booted next when it's full.
 };
+*/
 
-struct LRU: public PRA{
+/*struct LRU: public PRA{
 public:
   Word findVictim();
   LRU();
@@ -42,18 +58,19 @@ public:
 private:
   LRUList;
 };
+*/
 
 class MM{ //Memory Manager for OS
-private:
-  freeFrames(); //which frames are free, if any??
-  MM(); //constructor??
-  PCB pageTable;
-public:
-  MM(); //constructor creates pcb
-  instance();//?
-  operator=();//?
-  void pageIn(pageTable); //bring in page via demand paging, see 10.2 in book
-};
+     private:
+        freeFrames(); //which frames are free, if any??
+        MM(); //constructor??
+        PCB pageTable;
+     public:
+        MM(); //constructor creates pcb
+        instance();//?
+        operator=();//?
+        void pageIn(pageTable); //bring in page via demand paging, see 10.2 in book
+ };
 
 struct PTE{ //Page Table Entry
   unsigned char frameNumber;
