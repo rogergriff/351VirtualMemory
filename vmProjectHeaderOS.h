778bb4b2046fb437dea5c6be6166b3b9e55af064
jimenamurillo <32205837+jimenamurillo@users.noreply.github.com>
2019-05-10 15:20:23 -0700

    Update vmProjectHeaderOS.h

--- vmProjectHeaderOS.h
+++ vmProjectHeaderOS.h
@@ -9,63 +9,68 @@ using namespace std;
 //this will be the header file for the OS part our project
 
 
-/*class PRA{ //Page replacement algorithm
-      struct FIFO {
-        public:
-           Word findVictim();
-           FIFO(); // constructor
+/*
+//PAGE REPLACEMENT ALGORITHMS
+class PRA
+{ 	
+	struct FIFO {
+        	public:
+           		Word findVictim();
+           		FIFO(); // constructor
+        	private:
+          		 Word front; // what's at the front and will therefore be booted next when it's full.
+      	};
+  
+      	struct LRU: public PRA{
+		public:
+      			Word findVictim();
+      			LRU();
+      			void updateUsage();
+       		private:
+     			LRUList;
+      	};
+  
         private:
-          
-           Word front; // what's at the front and will therefore be booted next when it's full.
-      };
-  
-      
-      struct LRU: public PRA{
-       public:
-      Word findVictim();
-      LRU();
-      void updateUsage();
-  
-       private:
-      LRUList;
-      };
-  
-        
-    private:
-          int options(); //use this to get the type of PRA wanted at run-time, so something like "what page replacemnt algorithm do you want to use, 1 for fifo or 2 for lru
-    protected:
-          PRA(); //constructor for class
-    public:
-          ~PRA(); //destructor
-           Word findVictim();
-           getPRA();
-           updateUsage();
+        	int options(); //use this to get the type of PRA wanted at run-time, so something like "what page replacemnt algorithm do you want to use, 1 for fifo or 2 for lru
+    	protected:
+        	PRA(); //constructor for class
+	public:
+        	~PRA(); //destructor
+        	Word findVictim();
+        	getPRA();
+        	updateUsage();
   };
 */
 
 
-struct PTE{ //Page Table Entry
-  int frameNumber = 0;
-  bool valid = false;
+//PAGE TABLE ENTRIES
+struct PTE{ 
+	int frameNumber = 0;
+	bool valid = false;
 };
 
 
-struct PCB{ //Process control block
-  PTE myPageTable[256];
+
+//PROCESS CONTROL BLOCK
+struct PCB{ 
+	PTE myPageTable[256];
 }; //usually would contain more, but this is all we care about for the project
 
 
-class MM{ //Memory Manager for OS
-     private:
-      //int freeFrames(); //which frames are free, if any??
-       // MM(); //constructor??
-        PCB pageTable;
-     public:
-        MM(); //constructor creates pcb
-	int readPageTable(int, MemoryManagementUnit&, RAM&);
-        //MM instance();//?
-        //bool operator=(); //?
-        void pageIn(PTE[], int,RAM&); //bring in page via demand paging, see 10.2 in book
+
+
+//MEMORY MANAGER FOR OS
+class MM{ 
+	private:
+		//int freeFrames(); //which frames are free, if any??
+		// MM(); //constructor??
+        	PCB pageTable;
+	public:
+        	MM(); //constructor creates pcb
+		int readPageTable(int, MemoryManagementUnit&, RAM&);
+        	//MM instance();//?
+        	//bool operator=(); //?
+        	void pageIn(PTE[], int,RAM&); //bring in page via demand paging, see 10.2 in book
 };
 
 #endif
