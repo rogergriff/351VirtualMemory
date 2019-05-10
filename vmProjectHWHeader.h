99d1feea9693363842d51b326d9d77d3dd565e30
rogergriff <rogergriff121@gmail.com>
2019-05-10 12:28:52 -0700

    Update vmProjectHWHeader.h
    changing to most recent working version

--- vmProjectHWHeader.h
+++ vmProjectHWHeader.h
@@ -33,6 +33,7 @@ class MemoryManagementUnit{
 		int pageFaults();
 		void addPageAccesses();
 		void addPageFaults();
+
 	
 //void read<T>();
 		//int TLB_Accesses();
@@ -40,7 +41,7 @@ class MemoryManagementUnit{
 		//void addTLB_Accesses();
 		//void addTLB_Faults();
 };
-*/
+
 //class PageFault: public MMU{ 
 //	// private:
 //	public:
@@ -54,18 +55,13 @@ struct Word {
 	int u_int;
 	int uin32_t(uint32_t);
 	
-	/*struct address {
-		//Word address(); //we need to rename this somehow so it doesnt throw up an error constantly
-		int offset(int, int[]);
-		Word frame();
-		int page(int, int[]);
-	};*/
+
 };
 			
-	struct address {
+struct address {
 		//Word address(); //we need to rename this somehow so it doesnt throw up an error constantly
 		Word offset(int, vector<uint32_t>);
-		Word frame();
+		//Word frame();
 		Word page(int, vector<uint32_t>);
 	};
 			
@@ -90,7 +86,7 @@ struct Word {
 //};
 /*
 //Backing Store
-class BackingStore{
+class BackingStore{ //loads the contents of BACKING_STORE.bin to the program at the start i think
 	private: 
 		BackingStore();
 		
@@ -104,35 +100,32 @@ class BackingStore{
 	void read();
 };
 */
+struct Frame{
+	unsigned char data[256] = {};
+};
 
-/*
-class RAM{
-	struct Status {
-		bool accessed;
-		bool dirty;
-	}vmProjectHWHeader //? whats this for?
+struct Status{
+	int frameNumber;
+	bool accessed = false;
+	bool dirty = true;
+};
+
+class RAM{ //where we're storing our frames/frames table i think
 	private:
-		RAM();
+		//RAM();
 		int frameSize = 256;
+		Frame framesTable[256];
+		Status arrStatus[256];
 	public:
 		RAM();
-		RAM instance();
-		RAM operator=();
-		void read<T>();
-		Status arrStatus [Status, frameSize]
+		//RAM instance();
+		//RAM operator=();
+		//void read<T>();
+		
+		int checkArrStatusforDirty();
 };
-*/
 
-/*
-struct Frame{
-      unsigned char byt[255];
-};
-*/
 
-/*struct Status{
-	bool accessed;
-	Bool dirty;
-}*/
+
  
 #endif
- 
