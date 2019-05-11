93a9c3443e5e447d91fdf50d27a6409e312ad516
rogergriff <rogergriff121@gmail.com>
2019-05-10 22:37:02 -0700

    Update vmProjectHWHeader.h

--- vmProjectHWHeader.h
+++ vmProjectHWHeader.h
@@ -7,6 +7,68 @@ using namespace std;
 
 
 // Memory Management Unit
+struct tlbEntry{
+int pageNumber = -1;
+int frameNumber = -1;
+};
+struct Frame{
+		void fillFrame(unsigned char[]);
+		unsigned char readFrame(int);
+		unsigned char data[256];
+	
+};
+
+struct Status{
+	int frameNumber;
+	bool accessed = false;
+	bool dirty = true;
+};
+
+struct Word { 
+	int u_int;
+	int uin32_t(uint32_t);
+	
+
+};
+			
+struct address {
+		//Word address(); //we need to rename this somehow so it doesnt throw up an error constantly
+		Word offset(uint32_t);
+		//Word frame();
+		Word page(uint32_t);
+	};
+
+class BackingStore{ //loads the contents of BACKING_STORE.bin to the program at the start i think
+	private: 
+		//BackingStore();
+		
+	public:
+	
+	BackingStore();
+	~BackingStore();  //destructor
+//	BackingStore instance();  //  in main.cpp:
+ 		//	BS BS::instance(  ){} 
+//	BackingStore operator=();
+	void read(int, Frame[]);
+};
+	
+class RAM{ //where we're storing our frames/frames table i think
+	private:
+		//RAM();
+		int frameSize = 256;
+		Frame framesTable[256];
+		Status arrStatus[256];
+	public:
+		RAM();
+		//RAM instance();
+		//RAM operator=();
+		unsigned char read(int,int);
+		int FreeFrames(int, BackingStore);
+		//void frameIn(int);
+};
+
+
+
 class MemoryManagementUnit{ 
 	/*struct PageFault {
 		PageFault();
@@ -18,52 +80,41 @@ class MemoryManagementUnit{
 	//	MemoryManagementUnit();
 		int Page_AccCount;
 		int Page_Faults;
-	
-		//TLB TLBtable;
-	
-		//int TLB_AccCount;
-		//int TLB_Faults;
+		tlbEntry tBuffer[16]; //TLB
+		int TLB_AccCount;
+		int TLB_Faults;
+		void clearTLB(); //clear the buffer
 	public:
-		//void clearTLB(); //clear the buffer
 		MemoryManagementUnit(); //initialize?
 	
 		//MemoryManagementUnit( , );
-	
-		//MemoryManagementUnit instance();
-		//MemoryManagementUnit operator=();
+	//
+	//	MemoryManagementUnit instance();
+	//	MemoryManagementUnit operator=();
 		int pageAccesses();
 		int pageFaults();
 		void addPageAccesses();
 		void addPageFaults();
+		bool inTLB(int);
+		int TLBAccesses();
+		int TLBFaults();
+		void addTLBAccesses();
+		void addTLBFaults();
+		void Replace(tlbEntry);
+		void updateUsageLRU(tlbEntry);
+		//tlbEntry readPageTable(int, MemoryManagementUnit&, RAM&);
+		int readTLBTable(int);
+
+//void pageIn(TLBentries[]. int, RAM&)
+
 
 	
-		//void read<T>();
-	
-		//TLB stuff
-		//int readTLBtable(int, Ram& );
+//void read<T>();
 		//int TLB_Accesses();
 		//int TLB_Faults();
-		//void addTLBaccesses();
-		//void addTLBfaults();
-		//void pageIn(TLBentries[]. int, RAM&);
-	
+		//void addTLB_Accesses();
+		//void addTLB_Faults();
 };
-/*
-more TLB stuff
-
-struct TLBentries{
-	int pageNumber;
-	int frameNumber;
-};
-
-struct TLB{
-	TLBentries myTLBtable[16];
-};
-
-
-*/
-
-
 
 //class PageFault: public MMU{ 
 //	// private:
@@ -74,20 +125,7 @@ struct TLB{
 
 //Address nested in Word Struct
 
-struct Word { 
-	int u_int;
-	int uin32_t(uint32_t);
-	
-
-};
-			
-struct address {
-		//Word address(); //we need to rename this somehow so it doesnt throw up an error constantly
-		Word offset(int, vector<uint32_t>);
-		//Word frame();
-		Word page(int, vector<uint32_t>);
-	};
-			
+		
 
 
 //// Word
@@ -107,46 +145,10 @@ struct address {
 //	Word page();		
 
 //};
-/*
+
 //Backing Store
-class BackingStore{ //loads the contents of BACKING_STORE.bin to the program at the start i think
-	private: 
-		BackingStore();
-		
-	public:
-	
-	BackingStore();
-	~BackingStore();  //destructor
-	BackingStore instance();  //  in main.cpp:
- 		//	BS BS::instance(  ){} 
-	BackingStore operator=();
-	void read();
-};
-*/
-struct Frame{
-	unsigned char data[256] = {};
-};
 
-struct Status{
-	int frameNumber;
-	bool accessed = false;
-	bool dirty = true;
-};
 
-class RAM{ //where we're storing our frames/frames table i think
-	private:
-		//RAM();
-		int frameSize = 256;
-		Frame framesTable[256];
-		Status arrStatus[256];
-	public:
-		RAM();
-		//RAM instance();
-		//RAM operator=();
-		//void read<T>();
-		
-		int checkArrStatusforDirty();
-};
 
 
 
