6a12604feff40c92edf637a4454f14a8cf07da23
jimenamurillo <32205837+jimenamurillo@users.noreply.github.com>
2019-05-11 16:47:39 -0700

    Update vmProjectHeaderOS.h

--- vmProjectHeaderOS.h
+++ vmProjectHeaderOS.h
@@ -26,37 +26,36 @@ using namespace std;
 
 
 /*class PRA{ //Page replacement algorithm
-      struct FIFO {
-        public:
-           Word findVictim();
-           FIFO(); // constructor
-        private:
-          
-           Word front; // what's at the front and will therefore be booted next when it's full.
-      };
+	struct FIFO {
+		public:
+			Word findVictim();
+			FIFO(); // constructor
+		private:
+			Word front; // what's at the front and will therefore be booted next when it's full.
+	};
   
-      
-      struct LRU: public PRA{
-       public:
-      Word findVictim();
-      LRU();
-      void updateUsage();
-  
-       private:
-      LRUList;
-      };
-  
-        
-    private:
-          int options(); //use this to get the type of PRA wanted at run-time, so something like "what page replacemnt algorithm do you want to use, 1 for fifo or 2 for lru
-    protected:
-          PRA(); //constructor for class
-    public:
-          ~PRA(); //destructor
-           Word findVictim();
-           getPRA();
-           updateUsage();
-  };
+
+	struct LRU: public PRA{
+		public:
+			Word findVictim();
+			LRU();
+			void updateUsage();
+		private:
+			LRUList;
+	};
+  	
+	private:
+		int options(); 	//use this to get the type of PRA wanted at run-time, 
+				//so something like "what page replacemnt algorithm do
+				//you want to use, 1 for fifo or 2 for lru
+	protected:
+		PRA(); //constructor for class
+	public:
+		~PRA(); //destructor
+		Word findVictim();
+		getPRA();
+		updateUsage();
+};
 */
 
 
@@ -78,13 +77,13 @@ class MM{
 	private:
 		//int freeFrames(); //which frames are free, if any??
 		// MM(); //constructor??
-		PCB pageTable;
+		PCB pageTable;		//page table type PCB
 	public:
-		MM(); //constructor creates pcb
-		tlbEntry readPageTable(int, MemoryManagementUnit&, RAM&,BackingStore&);
+		MM();			//constructor creates pcb
+		tlbEntry readPageTable(int, MemoryManagementUnit&, RAM&,BackingStore&);	//reads the page table entry
 		//MM instance();//?
 		//bool operator=(); //?
-		void pageIn(PTE[], int, RAM&, BackingStore&); //bring in page via demand paging, see 10.2 in book
+		void pageIn(PTE[], int, RAM&, BackingStore&); //bring in page via demand paging (10.2 in book)
 };
 
 
